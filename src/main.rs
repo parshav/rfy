88172b06c98d5a7075a2146df8990807b76a4697
Parshav Chauhan <pchauhan@clientresourcesinc.com>
2019-07-21 22:57:29 -0500

    Yew added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+
+struct Model {}
+
+enum Msg {
+    DoIt,
+}
+
+impl Component for Model {
+    // Some details omitted. Explore the examples to see more.
+
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model {}
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::DoIt => {
+                // Update your model on events
+                true
+            }
+        }
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <h1> {"Hey ! "} </h1>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
